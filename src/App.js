d4db3bf8b440a497e29fcf807b830ac507dc30c7
Eduardo Lopez <ed98lopez@gmail.com>
2021-02-06 11:29:27 -0800

    trying to turn in the project

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ const App = () => {
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  // sync up with, if any.......Seeing if this next push will submit the project with codeGrade.
 
 
   const [chars, setChars]= useState([])
